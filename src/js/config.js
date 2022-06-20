66e68f652dd920ca36890abdd4c9729eace937a7
Balogh Bendegúz <tomivalmosok@gmail.com>
2022-06-20 21:27:44 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '5318358d-9f69-48da-86cb-1042851488e3';
+export const MODAL_CLOSE_SEC = 2.5;
